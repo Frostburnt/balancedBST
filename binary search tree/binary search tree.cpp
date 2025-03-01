12393402b3684eb4e14de97bf41613b969c896fa
Frostburn (James) <tonjesjames@gmail.com>
2025-03-01 21:14:31 +0100

    Added in an insertion method
    currently insertion method does not balance the list automatically.
    
    There are some junk files in here that I will delete

--- binary search tree/binary search tree.cpp
+++ binary search tree/binary search tree.cpp
@@ -1,27 +1,53 @@
-
-
+//notes
+//decided to use floats, as with a randomly generated num
+//things to remember is left is always smaller, right is always larger than the current node
 #include <iostream>
 
 
 struct Node {
     Node* left = NULL;      //smaller child
     Node* right = NULL;     //bigger child
-    //Node* parent = NULL;    //
+    //Node* parent = NULL;  //
     float key;              //value
 
-    Node(float value) {
+    Node(float value) {        
         key = value;
+    }
+    Node* operator [](int i) {
+        switch (i) {    //probably don't need break statements if 
+        case 0:
+            return left;
+            break;
+        case 1:
+            return right;
+            break;
+        default:
+            return NULL;
+        }
     }
 };
 
 struct BST {                //member functions need to be filled out
     Node* head;
-    BST(Node *node) {
+    BST(Node *node = NULL) {
         head = node;
     }
     
-    void insert(Node node) {
-
+    void insert(Node *node) {                   //we do this here for the first step of insertion so that we can pass in the head
+        head = insertHelper(head, node);        //then we treaat subsequent nodes as if they are heads to other binary search trees
+    }
+    Node *insertHelper(Node *root, Node *node) {    //I used "root" instead of "head" here to avoid confusion with the Tree's head, and the current sub-trees head
+        float key = node->key;
+        if (root == NULL) {                          
+            return node;
+        }
+        else if (key < root->key) {                                 //we change where our current working root node is based on where it belongs, left or right
+            root->left = insertHelper(root->left, node);
+        }
+        else {
+            root->right = insertHelper(root->right, node);
+        }
+        return root;
     }
     void remove(float value) {
 
@@ -44,21 +70,24 @@ Node* nMax(Node* node) {                 //returns the right most node, which wi
 }
 Node* nMin(Node* node) {                 //returns the lowest left side node, which will be the lowest value
     Node* working = node;
-    while (working->left != NULL) {
+    while (working->left != NULL) {     //simply loops until there is no next node, this means we've reached the end
         working = working->left;
     }
     return working;
 }
 
-Node* BSearch(Node* root, float key){
+Node* bSearch (Node* root, float key){
     return NULL;
 }
-unsigned int nSize(Node node){      //returns the size of the sub-tree at this node, inclusive
-    unsigned int total = 1;            
+
+
+
+unsigned int nSize(Node node){          //returns the size of the sub-tree at this node, inclusive. This is the "C variable" in the assigned task
+    unsigned int total = 1;             //it takes this nodes count (1)
     if (node.left != NULL)
-        total += nSize(*node.left);
+        total += nSize(*node.left);     //and recursively takes the left branch
     if (node.right != NULL)
-        total += nSize(*node.right);
+        total += nSize(*node.right);    //and right branch's totals too
     return total;
 }
 
@@ -67,6 +96,11 @@ BST test(&onlyNode);
 
 
 int main(){
-    std::cout << test.head->key;
+    Node a(5.3);
+    test.insert(&a);
+
+    std::cout << test.head->key << std::endl;
+    //std::cout << test.head->left->key << std::endl;
+    std::cout << test.head->right->key << std::endl;
     
 }
